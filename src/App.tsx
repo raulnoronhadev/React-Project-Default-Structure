e0949169554cd194f411b54b5e1ec853dd57f853
raul <raulnoronhadev@gmail.com>
2025-10-26 16:22:12 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { CssBaseline, ThemeProvider } from '@mui/material';
+import { ColorModeContext, useMode } from './theme';
+
+export default function App() {
+
+  return (
+    <>
+      <h1>Raul Noronha</h1>
+    </>
+  )
+}
